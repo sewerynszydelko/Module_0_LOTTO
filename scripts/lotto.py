42ce6dcd7800c0c34b6cb8a2bcad22690979aa2e
sewerynszydelko <seweryn494a@gmail.com>
2024-02-06 22:45:50 +0100

    Refactor funcion to give sets not list , modyfing test , deleate some unnecessary virables

--- scripts/lotto.py
+++ scripts/lotto.py
@@ -1,38 +1,44 @@
 import random
 
 
-user_nums = [49, 22, 35, 7, 5, 9]
+def get_random_6_nums() -> set:
+    return set([random.randint(1, 49) for i in range(6)])
 
 
-def get_random_6_nums() -> list:
-    return [random.randint(1, 49) for i in range(6)]
-
-
-def get_user_num():
+def get_user_num() -> set:
     while True:
         try:
             user_input = list(
                 map(int, input("Pleas enter 6 nums like this:'1 2 20 30  enter': ").split(" ")))
-            if len(user_input) > 6:
+            if len(set(user_input)) > 6:
                 print("You give me too many numbers i need only 6")
                 raise ValueError
-            elif len(user_input) < 6:
-                print("Not enought numbers i need 6")
+            elif len(set(user_input)) < 6:
+                print("Not enought numbers i need 6\n and only orginal no duplicate!")
                 raise ValueError
             break
         except ValueError as e:
             print("something went wrong:\n", e)
-    return user_input
+    return set(user_input)
 
 
 def game():
-    guessing = True
+    count_trys = 0
+    user_choice = get_user_num()
 
-    while guessing:
+    while True:
+        count_trys += 1
         curent_nums_random = get_random_6_nums()
-        user_choice = get_user_num()
 
-        break
+        if user_choice == curent_nums_random:
+            print(f"congratulation it took's {count_trys} trys")
+            break
+
+        if count_trys == 10000000:
+            print(f"It's took already {count_trys} trys")
+            break
+
+    return print("It cost a lot of time")
 
 
 if __name__ == "__main__":
--- test/lotto_test.py
+++ test/lotto_test.py
@@ -2,10 +2,13 @@ import sys
 import os
 sys.path.append(os.path.abspath(
     "C:\\Users\\5792\\Desktop\\Module_0_Lotto\\scripts"))
-from lotto import get_random_6_nums
+from lotto import get_random_6_nums, get_user_num
 
 
 def test_get_random_6_nums_list():
     random_6_list = get_random_6_nums()
 
-    assert type(random_6_list) is list
+    assert type(random_6_list) is set
+
+def test_get_user_num_is_list():    
+    assert type(get_user_num()) is set
