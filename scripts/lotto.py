7a5dd73704f0c43e8fd1267bada36df3de01265b
sewerynszydelko <seweryn494a@gmail.com>
2024-02-06 21:55:39 +0100

    Take funcion to get user num from loop game and create funcion

--- scripts/lotto.py
+++ scripts/lotto.py
@@ -8,25 +8,29 @@ def get_random_6_nums() -> list:
     return [random.randint(1, 49) for i in range(6)]
 
 
+def get_user_num():
+    while True:
+        try:
+            user_input = list(
+                map(int, input("Pleas enter 6 nums like this:'1 2 20 30  enter': ").split(" ")))
+            if len(user_input) > 6:
+                print("You give me too many numbers i need only 6")
+                raise ValueError
+            elif len(user_input) < 6:
+                print("Not enought numbers i need 6")
+                raise ValueError
+            break
+        except ValueError as e:
+            print("something went wrong:\n", e)
+    return user_input
+
+
 def game():
     guessing = True
 
     while guessing:
         curent_nums_random = get_random_6_nums()
-
-        while True:
-            try:
-                user_input = list(
-                    map(int, input("Pleas enter 6 nums like this:'1 2 20 30  enter': ").split(" ")))
-                if len(user_input) > 6:
-                    print("You give me too many numbers i need only 6")
-                    raise ValueError
-                elif len(user_input) < 6:
-                    print("Not enought numbers i need 6")
-                    raise ValueError
-                break
-            except ValueError as e:
-                print("something went wrong:\n", e)
+        user_choice = get_user_num()
 
         break
 
