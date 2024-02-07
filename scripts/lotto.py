9e093c9dfa66320cc5599f343cca2b29ffa34e20
sewerynszydelko <seweryn494a@gmail.com>
2024-02-07 09:50:20 +0100

    Create func for info to user about years and cost , make it test

--- scripts/lotto.py
+++ scripts/lotto.py
@@ -1,8 +1,10 @@
 import random
 
+ONE_GAME_PRICE = 3
+WEEKS_IN_YEAR = 52
 
 def get_random_6_nums() -> set:
-    return set([random.randint(1, 49) for i in range(6)])
+    return set(random.randint(1, 49) for i in range(6))
 
 
 def get_user_num() -> set:
@@ -14,13 +16,21 @@ def get_user_num() -> set:
                 print("You give me too many numbers i need only 6")
                 raise ValueError
             elif len(set(user_input)) < 6:
-                print("Not enought numbers i need 6\n and only orginal no duplicate!")
+                print("Not enought numbers i need 6\nand only orginal no duplicate!")
                 raise ValueError
             break
         except ValueError as e:
             print("something went wrong:\n", e)
     return set(user_input)
 
+def cost_and_years_to_win(count_trys) -> str:
+
+    cost_game = count_trys * ONE_GAME_PRICE
+    how_many_years_to_win = count_trys // WEEKS_IN_YEAR
+
+    final_info = f"If we asume you will play once in week you need a {how_many_years_to_win:,} years and it will cost you {cost_game:,} zł if 1 game = 3 zł"
+
+    return final_info
 
 def game():
     count_trys = 0
@@ -31,14 +41,16 @@ def game():
         curent_nums_random = get_random_6_nums()
 
         if user_choice == curent_nums_random:
-            print(f"congratulation it took's {count_trys} trys")
+            print(f"congratulation it took's {count_trys} trys\n")
             break
 
         if count_trys == 10000000:
-            print(f"It's took already {count_trys} trys")
+            print(f"It's took already {count_trys} trys\n")
             break
+    
+    final_info = cost_and_years_to_win(count_trys)
 
-    return print("It cost a lot of time")
+    return print(final_info)
 
 
 if __name__ == "__main__":
--- test/lotto_test.py
+++ test/lotto_test.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(
     "C:\\Users\\5792\\Desktop\\Module_0_Lotto\\scripts"))
-from lotto import get_random_6_nums, get_user_num
+from lotto import get_random_6_nums, get_user_num, cost_and_years_to_win
 
 
 def test_get_random_6_nums_list():
@@ -12,3 +12,9 @@ def test_get_random_6_nums_list():
 
 def test_get_user_num_is_list():    
     assert type(get_user_num()) is set
+
+def test_cost_and_years_to_win():
+    count_trys_temporaly = 100
+    info = cost_and_years_to_win(count_trys=count_trys_temporaly)
+
+    assert type(info) is str
